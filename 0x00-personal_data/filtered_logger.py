0edbf6cc0f7359411b84d641ef4ba53700af7921
elishambadi <embadi43@gmail.com>
2023-03-02 15:36:08 +0300

    incl separator

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,6 @@ def filter_datum(fields: list, redaction: str, message: str, separator: str) -> 
     """This filters the log message using a RegEx
     """
     for i in range(len(fields)):
-        password_regex = rf'({re.escape(fields[i])}=)[^;]+(;)'
+        password_regex = rf'({re.escape(fields[i])}=)[^{re.escape(separator)})]+({re.escape(separator)})'
         message = re.sub(password_regex, r'\1xxx\2', message)
     return message
