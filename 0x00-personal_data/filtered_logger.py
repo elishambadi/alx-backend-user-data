5e1e949ec97ec8f17d835cfc7b0fa1490b7b5e12
elishambadi <embadi43@gmail.com>
2023-03-02 15:32:40 +0300

    reduce lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,7 @@ import re
 def filter_datum(fields: list, redaction: str, message: str, separator: chr):
     """This filters the log message using a RegEx
     """
-    x = message
     for i in range(len(fields)):
         password_regex = rf'({re.escape(fields[i])}=)[^;]+(;)'
-        x = re.sub(password_regex, r'\1xxx\2', x)
-    return x
+        message = re.sub(password_regex, r'\1xxx\2', message)
+    return message
