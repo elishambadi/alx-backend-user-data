578700bc06432afee3402c73e0c52aea2188cc37
elishambadi <embadi43@gmail.com>
2023-03-02 17:21:59 +0300

    add comments

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,10 +14,16 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        """Initialization function
+           Example:
+           formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Formats code based on the FORMAT
+        """
         x = super().format(record).split()
         msg = filter_datum(self.fields, self.REDACTION, x[5], self.SEPARATOR)
         x.pop(5)
