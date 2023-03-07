900e7be6d5f3a16b87286bb291f3c0243f8a4872
elishambadi <embadi43@gmail.com>
2023-03-07 19:01:04 +0300

    add task 6 - extract from auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
 """Basic Auth inheriting from Auth
 """
+import base64
+import re
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic Authentication class
     """
-    pass
+    def extract_base64_authorization_header(self, auth_header: str) -> str:
+        """Extracts and return base 64 part of the code
+           e.g. >> str = "Authorization: Basic afsfnkwnkjfnknrfi23u4uh92j"
+                >> auth = BasicAuth()
+                >> print(auth.extract_base64_authorization_header(str))
+                >> "afsfnkwnkjfnknrfi23u4uh92j"
+        """
+        if type(auth_header) != str:
+            return None
+        else:
+            match = re.search(r'^Basic\s+(.*)$', auth_header)
+
+            if match:
+                return match.group(1)
+            else:
+                return None
