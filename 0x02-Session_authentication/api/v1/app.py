4e936df392fc9e2d5b95663691ae52746a2b9754
elishambadi <embadi43@gmail.com>
2023-03-09 17:08:21 +0300

    fix multiline statement

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -41,7 +41,7 @@ def auth_before():
         pass
     else:
         if auth.require_auth(request.path, urls) is True:
-            if auth.authorization_header(request) is None
+            if auth.authorization_header(request) is None\
             and auth.session_cookie(request) is None:
                 print("--- auth.authorization_header(request)")
                 abort(401)
