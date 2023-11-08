06a85ab2dc2047b34a6abc949c7e9c36fb743e50
elishambadi <embadi43@gmail.com>
2023-11-08 19:09:37 +0300

    extract auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,3 +10,20 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """BasicAuth that inherits from Auth"""
     pass
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        """
+            returns the Base64 part of the Authorization header
+        """
+
+        if authorization_header is None:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        elif authorization_header.startswith("Basic ") is False:
+            return None
+        else:
+            return authorization_header[6:]
