f6197aec62b4581150a738f42e7245bb4ee942d0
elishambadi <embadi43@gmail.com>
2023-11-16 11:04:40 +0300

    create user route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python3
 """App module"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
 def index():
     """Home Route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=["POST"])
+def users():
+    """
+    Route to register a user
+    Implements: register_user from auth
+    """
+    email = request.form.get("email")
+    password = request.form.get("email")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({
+            "email": f"{user.email}",
+            "message": "user created"
+            }), 201
+    except Exception as exc:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
