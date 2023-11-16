56ec621416a9ea6820f1a6ed3984894fde76034d
elishambadi <embadi43@gmail.com>
2023-11-16 17:19:17 +0300

    fix logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -64,20 +64,35 @@ def login():
 @app.route('/sessions', methods=["DELETE"])
 def logout():
     """Logs Out a User"""
+
     session_id = request.cookies.get("session_id")
-
-    if session_id is None:
-        return redirect(url_for('index'))
-
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        response = redirect(url_for('index'))
-        # Set a cookie in the response with immediate expiry
-        response.set_cookie('session_id', '', expires=0)
-        return response
-    else:
+    if user is None:
         abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
+
+    # session_id = request.cookies.get("session_id")
+
+    # if session_id is None:
+    #     return redirect(url_for('index'))
+
+    # user = AUTH.get_user_from_session_id(session_id)
+    # if user:
+    #     AUTH.destroy_session(user.id)
+    #     response = redirect(url_for('index'))
+    #     # Set a cookie in the response with immediate expiry
+    #     response.set_cookie('session_id', '', expires=0)
+    #     return response
+    # else:
+    #     abort(403)
+
+    # session_id = request.cookies.get("session_id")
+    # user = AUTH.get_user_from_session_id(session_id)
+    # if user is None:
+    #     abort(403)
+    # AUTH.destroy_session(user.id)
+    # return redirect("/")
 
 
 if __name__ == "__main__":
